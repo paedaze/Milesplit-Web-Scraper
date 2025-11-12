a40563020c7edf6a6ce8150d12a8f5c3554a3579
Aaron Pae <119465294+paedaze@users.noreply.github.com>
2025-11-11 23:38:38 -0500

    Refactor scraper.py for athlete data scraping

--- scraper.py
+++ scraper.py
@@ -1,8 +1,3 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from scrapy import spiders
 from enum import Enum
 
@@ -15,54 +10,19 @@ class Event(Enum):
     FIVE_THOUSAND = '5000 Meter Run'
     THIRTYTWO_HUNDRED = '3200 Meter Run'
     SIXTEEN_HUNDRED = '1600 Meter Run'
+    FIFTEEN_HUNDRED = '1500 Meter Run'
+    TWELVE_HUNDRED = '1200 Meter Run'
     EIGHT_HUNDRED = '800 Meter Run'
     FOUR_HUNDRED = '400 Meter Run'
     TWO_HUNDRED = '200 Meter Run'
     ONE_HUNDRED = '100 Meter Run'
 
 html_paths = {
-    'options_id' : 'postHeaderSearchCategory',
-    'search_school_id' : 'postHeaderSearch',
-    'search_button_class' : 'input-group-append',
-    'sport_dropdown_id' : 'rosterFilterType',
     'roster_parent_xpath' : "//ul[@id='rosterDataset']",
     'student_info_xpath' : "div[@class='data-point w-30 w-md-50 d-flex align-items-center']/a",
-    'student_record_table_xpath' : "//div[@class='record-box']/table/tbody",
     'student_event_time' : "//td[@class='event']",
     'student_name_xpath' : "//h1[@class='athlete-name']/text()"
 }
-
-# Function to dynamically get the school page link from the search results
-def get_school_link(school_name):
-    # Set up the driver
-    op = webdriver.ChromeOptions()
-    op.add_argument("--headless=new")
-    driver = webdriver.Chrome(options=op)
-    # Accesses the search page of the website
-    driver.get("https://ga.milesplit.com/search")
-
-    # Selects the Teams option from the dropdown menu
-    dropdown = Select(driver.find_element(By.ID, html_paths['options_id']))
-    dropdown.select_by_value('team')
-
-    # Finds and selects the school 
-    txtBox = driver.find_element(By.ID, html_paths['search_school_id'])
-    txtBox.send_keys(school_name)
-
-    # Submit the search form (clicks the 'search' button)
-    driver.find_element(By.CLASS_NAME, html_paths['search_button_class']).find_element(By.TAG_NAME, 'button').click()
-
-    # Find the link to the team page via the first search result (Waits for the search results to load, timeout given in the WebDriverWait parameter)
-    wait = WebDriverWait(driver, 10)
-    try:
-        link = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'result-description')))[0].get_attribute('href')
-    except:
-        driver.quit()
-        print("No results found")
-        return None
-
-    driver.quit()
-    return link + '/roster' # Returns the school roster link
 
 # Web spider for handling athlete data scraping
 # This spider scrapes the athlete data from the school page and stores it in 'athlete_info_output'
