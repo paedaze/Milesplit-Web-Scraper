843d776474635398d09e954ab198916e0c85c78b
Aaron Pae <119465294+paedaze@users.noreply.github.com>
2025-11-12 21:58:16 -0500

    Update gamilesplitschoolfinder.py

--- gamilesplitschoolfinder.py
+++ gamilesplitschoolfinder.py
@@ -1,16 +1,11 @@
-import pprint
+import sqlite3
 from scrapy import Request
 from scrapy import crawler
 from scrapy import spiders
 from scrapy.crawler import CrawlerProcess
 
-
-def dict_to_instantiable_string(dictionary):
-    # Return a copy/paste-ready assignment that recreates the provided dictionary
-    literal = pprint.pformat(dictionary, sort_dicts=True, width=100)
-    return f"copied_dict = {literal}"
-
-class get_ga_school_links(spiders.Spider):
+# Web spider for scraping all ga milesplit schools (school names and roster links)
+class gamilesplit_school_scraper(spiders.Spider):
     name = 'schools'
     start_urls = ["https://ga.milesplit.com/teams"]
 
@@ -22,13 +17,63 @@ class get_ga_school_links(spiders.Spider):
         table = response.xpath("//table[@class='teams order-table table']/tbody/tr/td/a")
         for school in table:
             school_name = school.xpath("text()").get(default="").strip()
-            if "null" not in school_name:
-                school_links[school_name] = school.attrib["href"]
+            if "null" in school_name:
+                continue
+            else:
+                self.school_links[school_name] = school.attrib["href"] + '/roster'
 
+# Function used to run the ga school milesplit data scraper and add the scraped data to the database
+def update_school_database():
+    # Connect to a database (or create a new one if it doesn't exist)
+    conn = sqlite3.connect('ga-milesplit-school-database.db')
+    cursor = conn.cursor()
 
-if __name__ == "__main__":
+    # Create a table
+    cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS gamilesplitschools (name TEXT, link TEXT)
+        '''
+    )
+
+    # Run web spider
     school_links = {}
     process = CrawlerProcess()
-    process.crawl(get_ga_school_links, school_links=school_links)
+    process.crawl(gamilesplit_school_scraper, school_links=school_links)
     process.start()
-    print(dict_to_instantiable_string(school_links))
+
+    # Insert data into database
+    for school_name, school_link in school_links.items():
+        cursor.execute(
+            '''
+            INSERT INTO gamilesplitschools (name, link)
+            VALUES (?, ?)
+            ''',
+            (school_name, school_link)
+        )
+
+    conn.commit()
+    conn.close()
+
+# FOR DEVELOPER USAGE: Used to read the database
+def read_school_database():
+    # Connect to a database (or create a new one if it doesn't exist)
+    conn = sqlite3.connect('ga-milesplit-school-database.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        '''SELECT name, link from gamilesplitschools'''
+    )
+
+    schools = cursor.fetchall()
+    for row in schools:
+        print(row)
+
+
+if __name__ == '__main__':
+    response = input(str('Do you want to update or read? (U/R): '))
+    if response.upper() == 'U':
+        update_school_database()
+    elif response.upper() == 'R':
+        read_school_database()
+    
+        
