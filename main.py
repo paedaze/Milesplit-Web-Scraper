896796f24204ceb5915f18fdb7444887d349bf8a
Aaron Pae <119465294+paedaze@users.noreply.github.com>
2025-11-17 23:41:52 -0500

    Enhance Flask app with CORS and new API endpoint
    Added CORS support and a new endpoint for processing virtual meets.

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from pathlib import Path
 import sqlite3
 
-from flask import Flask, jsonify, render_template
-from scrapy.crawler import CrawlerProcess
-
-import scraper
-from scraper import Sport, athlete_spider, HTML_PATHS
+from flask import Flask, jsonify, render_template, request
+from flask_cors import CORS
 
 
 # Function to convert string to enum
@@ -17,12 +13,12 @@ def string_to_enum(Enum, string):
 
 
 app = Flask(__name__)
-DATABASE_PATH = Path(__file__).resolve().parent / "ga-milesplit-school-database.db"
+CORS(app)
 
 
 def load_school_map():
     """Read the GA Milesplit school data from the SQLite database."""
-    with sqlite3.connect(DATABASE_PATH) as conn:
+    with sqlite3.connect('ga-milesplit-school-database.db') as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT name, link FROM gamilesplitschools ORDER BY name")
         schools = cursor.fetchall()
@@ -34,10 +30,21 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/api/schools")
+@app.route("/api/schools", methods=['GET'])
 def schools():
     return jsonify(load_school_map())
 
+@app.route("/api/process-virtual-meet", methods=['POST'])
+def simulate_meet():
+    data = request.json
+
+    for key, value in data.items():
+        print(f"{key}: {value}")
+
+    return jsonify({
+        'status': 'success',
+        'message': f'Successfully received items.'
+    }), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
