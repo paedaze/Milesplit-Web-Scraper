375a48f6681aeb7f9fb9bf4c3a46e4825e7e9688
Aaron Pae <119465294+paedaze@users.noreply.github.com>
2025-11-20 02:33:28 -0500

    Enhance simulate_meet with school names and scoring logic
    Updated the simulate_meet function to include school names and handle scoring for teams with less than 5 athletes in cross country.

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def convert_time(formatted_time):
         seconds += float(formatted_time)
     return seconds
 
+
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route("/")
 def index():
@@ -41,6 +41,7 @@ def load_schools():
         cursor.execute("SELECT name, link FROM gamilesplitschools ORDER BY name")
         schools = cursor.fetchall()
     return jsonify({name: link for name, link in schools})
+
 
 @app.route("/api/process-virtual-meet", methods=['POST'])
 def simulate_meet():
@@ -63,7 +64,7 @@ def simulate_meet():
         school_id = school_id_row[0]
         
         cursor.execute("""
-            SELECT a.name, e.event_name, r.time FROM results r
+            SELECT a.name, s.school_name, e.event_name, r.time FROM results r
             JOIN athletes a ON a.athlete_id = r.athlete_id
             JOIN events e ON e.event_id = r.event_id
             JOIN genders g ON g.gender_id = r.gender_id
@@ -71,23 +72,32 @@ def simulate_meet():
             WHERE s.school_id = ? AND e.event_name = ? AND g.gender = ?
         """, (school_id, selected_event, selected_gender))
 
-        # Get Top 7 athletes
+        # Get Top 7 athletes (Name, School, Event, Time, Points)
         meet_data = cursor.fetchall()
-        for entry in sorted(meet_data, key=lambda entry: convert_time(entry[2]))[:7]:
-            varsity_athletes.append(entry)
 
-     # Sort every varsity athlete from every school by time
-    varsity_athletes = sorted(varsity_athletes, key=lambda entry: convert_time(entry[2]))
+        sorted_school_athletes = sorted(meet_data, key=lambda entry: convert_time(entry[3]))[:7] # Sort a school's athletes and grab the top 7
+        if len(sorted_school_athletes) < 5 and selected_sport == Sport.CROSS_COUNTRY: # School is not valid for scoring if team has less than 5 athletes
+            for entry in sorted_school_athletes:
+                varsity_athletes.append(entry + (-1,))
+        else: # Otherwise, School is valid for scoring
+            for entry in sorted_school_athletes:
+                varsity_athletes.append(entry)
+
+    # Sort every varsity athlete from every school by time
+    varsity_athletes = sorted(varsity_athletes, key=lambda entry: convert_time(entry[3]))
 
     if selected_sport == Sport.CROSS_COUNTRY: # Cross Country scoring system
         for i in range(len(varsity_athletes)):
+            if len(varsity_athletes[i]) == 5: # If the athlete has already been scored as invalid, skip athlete
+                continue
             varsity_athletes[i] += (i+1,)
-    elif selected_sport == Sport.TRACK: # Track and Field scoring system
+    elif selected_sport == Sport.TRACK: # Track and Field scoring system: top 8 score, others score no points
         points = (10, 8, 6, 5, 4, 3, 2, 1)
         for i in range(len(varsity_athletes)):
-            if i == 8:
-                break
-            varsity_athletes[i] += (points[i],)
+            if i < 8:
+                varsity_athletes[i] += (points[i],) # Scoring athletes
+            else:
+                varsity_athletes[i] += (-1,) # Loser athletes
 
     for athlete_entry in varsity_athletes:
         print(athlete_entry)
